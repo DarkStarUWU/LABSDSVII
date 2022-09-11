680077edf06ab43b867239d0b50f50a852ed8881
DarkStarUWU <guevara4536@hotmail.com>
2022-09-11 14:46:11 -0500

    lab32 add

---
+++ LAB3/LAB32/lab32.php
@@ -0,0 +1,14 @@
+<?php
+    $precio1 = $_POST ['prec1'];
+    $precio2 = $_POST ['prec2'];
+    $precio3 = $_POST ['prec2'];
+
+    $media = ($precio1+$precio2+$precio3)/3;
+    echo "<br/> DATOS RECIBIDOS";
+    echo "<br/> Precio productos establecimiento 1: ". $precio1 . "dolares";
+    echo "<br/> Precio productos establecimiento 2: ". $precio2 . "dolares";
+    echo "<br/> Precio productos establecimiento 1: ". $precio3 . "dolares <br/>";
+    echo "<br/> Precio medio del producto es de: ". $media . "dolares";
+    
+    
+?>
